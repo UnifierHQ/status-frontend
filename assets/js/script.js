04de3909b486e7734b4d1f17084671086ea234c1
green <41323182+greeeen-dev@users.noreply.github.com>
2025-01-19 14:11:34 +0100

    Use new header style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,15 @@ const headers = new Headers({
     'Accept':'application/json'
 })
 
+const navbarData = {
+    "links": [
+        {
+            "href": "/",
+            "text": "Home"
+        }
+    ]
+}
+
 function apiPath(path) {
     return endpoint + path;
 }
@@ -357,7 +366,39 @@ function renderHistory(element, maximum) {
     }
 }
 
+function toggleNavbarList() {
+    // NOTE: this is not a linked list (linked lists are fake /j)
+    const linksList = document.getElementById('navbar-links-container');
+    const toggleElement = document.getElementById('navbar-mobile-toggle');
+
+    if (linksList.classList.contains('active')) {
+        linksList.classList.remove('active');
+        toggleElement.classList.remove('active');
+    } else {
+        linksList.classList.add('active');
+        toggleElement.classList.add('active');
+    }
+}
+
 function onLoad() {
+    // Navbar toggle
+    document.getElementById('navbar-mobile-toggle').addEventListener('click', toggleNavbarList);
+
+    // Set navbar
+    const navbarLinksElement = document.getElementById('navbar-links-container');
+
+    for (let i = 0; i < navbarData.links.length; i++) {
+        const link = navbarData.links[i];
+        const linkTemplate = document.getElementById('navbar-link-template')
+        const linkElement = linkTemplate.content.cloneNode(true);
+        const linkElementAnchor = linkElement.querySelector('a');
+        linkElementAnchor.href = link.href;
+        linkElementAnchor.innerHTML = link.text;
+
+        // Add element to navbar list
+        navbarLinksElement.appendChild(linkElement);
+    }
+
     //fetchStatus(); (backend is WIP)
     dummyStatus();
 }
